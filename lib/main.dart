12c478e1d49e52ec990ba9e3d37d763b7b348234
sao1269 <stephen.obrien@woodchoppersinn.com.au>
2020-05-09 22:24:57 +1000

    Second Change Main.Dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,14 +5,37 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  void answerQuestion(){
+    print('Answer Choosen !');
+  }
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favourite colour',
+      'What\'s your favorite animal',
+    ];
     return MaterialApp(
         home: Scaffold(
       appBar: AppBar(
         title: Text('My First App'),
       ),
-      body: Text('This is my default text'),
+      body: Column(
+        children: <Widget>[
+          Text('The Question'),
+          RaisedButton(
+            child: Text('Answer 1'),
+            onPressed: answerQuestion,
+          ),
+          RaisedButton(
+            child: Text('Answer 2'),
+            onPressed: answerQuestion,
+          ),
+          RaisedButton(
+            child: Text('Answer 3'),
+            onPressed: answerQuestion,
+          ),
+        ],
+      ),
     ));
   }
 }
