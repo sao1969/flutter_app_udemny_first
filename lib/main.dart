9bc1f863057da3eff3c712dac10a1a96b34f60b7
sao1269 <stephen.obrien@woodchoppersinn.com.au>
2020-05-10 20:55:10 +1000

    Revert "Seventh Change Main.Dart Updated main.dart with questions & answers in a MAP"
    This reverts commit 8096199d

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 import './question.dart';
-import './answer.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,7 +8,7 @@ void main() {
 
 class MyApp extends StatefulWidget {
 
-  @override
+@override
   State<StatefulWidget> createState() {
     // TODO: implement createState
     return _MyAppState();
@@ -29,34 +28,34 @@ class _MyAppState extends State <MyApp> {
   @override
   Widget build(BuildContext context) {
     var questions = [
-      {
-        'questionText': 'What\'s your favourite colour',
-        'questionAnswers': ['Black', 'Red', 'Green', 'White'],
-      },
-      {
-        'questionText': 'What\'s your favorite animal',
-        'questionAnswers': ['Dog', 'Cat', 'Rabbit', 'Horse'],
-      },
-      {
-        'questionText': 'What\'s your favourite car',
-        'questionAnswers': ['BMW', 'Benz', 'Audi', 'GoGoMobile'],
-      },
+      'What\'s your favourite colour',
+      'What\'s your favorite animal',
+      'What\'s your favourite car',
     ];
     return MaterialApp(
         home: Scaffold(
-          appBar: AppBar(
-            title: Text('My First App'),
+      appBar: AppBar(
+        title: Text('My First App'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Question(
+            questions[_questionIndex],
           ),
-          body: Column(
-            children: <Widget>[
-              Question(
-                questions[_questionIndex]['questionText'],
-              ),
-              Answer(_answerQuestion),
-              Answer(_answerQuestion),
-              Answer(_answerQuestion),
-            ],
-          ),)
-    );
+          RaisedButton(
+            child: Text('Answer 1'),
+            onPressed: _answerQuestion,
+          ),
+          RaisedButton(
+            child: Text('Answer 2'),
+            onPressed: _answerQuestion,
+          ),
+          RaisedButton(
+            child: Text('Answer 3'),
+            onPressed: _answerQuestion,
+          ),
+        ],
+      ),
+    ));
   }
 }
