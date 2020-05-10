9fe84ff0a15d84551e36e41dd82f1786bce7658d
sao1269 <stephen.obrien@woodchoppersinn.com.au>
2020-05-10 11:52:38 +1000

    Fourth Change Main.Dart Made main.dart file references _private

--- lib/main.dart
+++ lib/main.dart
@@ -9,18 +9,18 @@ class MyApp extends StatefulWidget {
 @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
-    return MyAppState();
+    return _MyAppState();
   }
 }
 
-class MyAppState extends State <MyApp> {
-  var questionIndex = 0;
+class _MyAppState extends State <MyApp> {
+  var _questionIndex = 0;
 
   void answerQuestion() {
     setState(() {
-      questionIndex = questionIndex + 1;
+      _questionIndex = _questionIndex + 1;
     });
-    print(questionIndex);
+    print(_questionIndex);
   }
 
   @override
@@ -38,7 +38,7 @@ class MyAppState extends State <MyApp> {
       body: Column(
         children: <Widget>[
           Text(
-            questions[questionIndex],
+            questions[_questionIndex],
           ),
           RaisedButton(
             child: Text('Answer 1'),
