3d51e1f5e8dd3b45244a11bb11c36e4a1a4fe887
sao1269 <stephen.obrien@woodchoppersinn.com.au>
2020-05-10 13:01:19 +1000

    Fifth Change Main.Dart Created new file question.dart and reference to main.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './question.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -16,7 +18,7 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State <MyApp> {
   var _questionIndex = 0;
 
-  void answerQuestion() {
+  void _answerQuestion() {
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
@@ -37,20 +39,20 @@ class _MyAppState extends State <MyApp> {
       ),
       body: Column(
         children: <Widget>[
-          Text(
+          Question(
             questions[_questionIndex],
           ),
           RaisedButton(
             child: Text('Answer 1'),
-            onPressed: answerQuestion,
+            onPressed: _answerQuestion,
           ),
           RaisedButton(
             child: Text('Answer 2'),
-            onPressed: answerQuestion,
+            onPressed: _answerQuestion,
           ),
           RaisedButton(
             child: Text('Answer 3'),
-            onPressed: answerQuestion,
+            onPressed: _answerQuestion,
           ),
         ],
       ),
