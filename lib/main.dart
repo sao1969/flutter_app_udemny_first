a7df026f02d153712b1d7b9bd1784da6cc9339bf
sao1269 <stephen.obrien@woodchoppersinn.com.au>
2020-05-10 21:17:34 +1000

    Lect 43 Before Maping changes answer.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import './question.dart';
+import './answer.dart';
 
 void main() {
   runApp(MyApp());
@@ -42,18 +43,9 @@ class _MyAppState extends State <MyApp> {
           Question(
             questions[_questionIndex],
           ),
-          RaisedButton(
-            child: Text('Answer 1'),
-            onPressed: _answerQuestion,
-          ),
-          RaisedButton(
-            child: Text('Answer 2'),
-            onPressed: _answerQuestion,
-          ),
-          RaisedButton(
-            child: Text('Answer 3'),
-            onPressed: _answerQuestion,
-          ),
+          Answer(_answerQuestion),
+          Answer(_answerQuestion),
+          Answer(_answerQuestion),
         ],
       ),
     ));
